a36888e7497ef5b7977171726e872dd7b84a1a67
Velin Kerkov <velin.kerkov@whitelotusorder.com>
2021-02-18 19:19:14 +0200

    OMG

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -21,25 +21,52 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     comments.push({
         id:commentId,
-        content
+        content,
+        status: 'pending'
     })
 
     commentsByPostId[req.params.id] = comments;
 
-    await axios.post('http://localhost:4005/events', {
+    const event = {
         type: 'CommentCreated',
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
-    })
+    };
+
+    // query service
+    await axios.post('http://localhost:4005/events', event);
+
+    // moderation service
+    await axios.post('http://localhost:4003/events', event);
 
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res)=> {
+app.post('/events', async (req, res)=> {
     console.log('Received Event: ', req.body.type);
+
+    const { type, data } = req.data;
+
+    if (type === "CommentModerated") {
+        const {postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => comment.id === id);
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
 
     res.send({});
 })

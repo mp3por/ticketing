da44b10c5ce7d87f11c37e628609dd33e4d6ee9e
Velin Kerkov <velin.kerkov@whitelotusorder.com>
2021-03-04 22:35:22 +0200

    refactor app start

---
+++ ticketing/auth/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+
+import {currentUserRouter} from "./routes/current-user";
+import {signUpRouter} from "./routes/signup";
+import {signInRouter} from "./routes/signin";
+import {signOutRouter} from "./routes/signout";
+import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
+import cookieSession from "cookie-session";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
+
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,36 +1,6 @@
-import express from 'express';
 import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-
-import {currentUserRouter} from "./routes/current-user";
-import {signUpRouter} from "./routes/signup";
-import {signInRouter} from "./routes/signin";
-import {signOutRouter} from "./routes/signout";
-import {errorHandler} from "./middlewares/error-handler";
-import {NotFoundError} from "./errors/not-found-error";
-import cookieSession from "cookie-session";
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import {app} from "./app";
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -43,15 +13,16 @@ const start = async () => {
             useUnifiedTopology: true,
             useCreateIndex: true
         });
-        console.log('Connected to MongoDB')
+        console.log('Connected to MongoDB');
     } catch (e) {
         console.log(e);
     }
+
+    app.listen(3000, ()=>{
+        console.log('Listening on 3000');
+    });
 };
 
 start();
 
-app.listen(3000, ()=>{
-    console.log('Listening on 3000');
-});
 

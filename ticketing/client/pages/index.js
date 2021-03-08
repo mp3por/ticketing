a686bfd65612001ca25aa13a932fd8cffb4317fc
Velin Kerkov <velin.kerkov@whitelotusorder.com>
2021-03-08 22:58:04 +0200

    normalize all the line endings

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,14 @@
-const Index = () => {
+import axios from "axios";
+
+const Index = ({currentUser}) => {
+    console.log(currentUser);
     return <h1>Landing page</h1>
+}
+
+Index.getInitialProps = async () => {
+    const response = await axios.get('/api/users/currentuser');
+
+    return response.data;
 }
 
 export default Index;

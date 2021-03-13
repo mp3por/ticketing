d6927784d4a563e0fba5775dd673d95c69a7ee03
Velin Kerkov <velin.kerkov@whitelotusorder.com>
2021-03-13 08:05:56 +0200

    Connecting to an service outside the namespace

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -5,17 +5,17 @@ const Index = ({currentUser}) => {
     return <h1>Landing page</h1>
 }
 
-Index.getInitialProps = async () => {
-    // const response = await axios.get('/api/users/currentuser');
+Index.getInitialProps = async ({req}) => {
     if (typeof window === 'undefined') {
-        
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {headers: req.headers}
+        );
+        return data;
     } else {
-        
+        const { data } = await axios.get('/api/users/currentuser');
+        return data;
     }
-    
-    return {};
-       
-    // return response.data;
 }
 
 export default Index;

161ab7633f872eb194fd1e9e96c40c3537bedf30
Velin Kerkov <velin.kerkov@whitelotusorder.com>
2021-03-13 08:42:25 +0200

    Created a header

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import buildClient from "../api/build-client";
 
-const Index = ({currentUser}) => {
+const LandingPage = ({currentUser}) => {
     console.log(currentUser);
     return currentUser ?  <h1>You are signed in </h1> : <h1>You are NOT signed in</h1>
 }
 
-Index.getInitialProps = async context => {
+LandingPage.getInitialProps = async context => {
     let client =  buildClient(context);
     const { data } = await client.get('/api/users/currentuser');
     return data;
 }
 
-export default Index;
+export default LandingPage;

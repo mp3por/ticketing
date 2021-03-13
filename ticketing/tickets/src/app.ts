1773794645bea416bbccb4dfca495d65a4c9fd5a
Velin Kerkov <velin.kerkov@whitelotusorder.com>
2021-03-13 09:50:36 +0200

    Setup of Tickets service in the whole architecture

---
+++ ticketing/tickets/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import {errorHandler, NotFoundError} from "@mp3por-tickets/common";
+import cookieSession from "cookie-session";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
+
---
+++ ticketing/tickets/src/index.ts
@@ -0,0 +1,28 @@
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import {app} from "./app";
+
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to MongoDB');
+    } catch (e) {
+        console.log(e);
+    }
+
+    app.listen(3000, ()=>{
+        console.log('Listening on 3000');
+    });
+};
+
+start();
+
+

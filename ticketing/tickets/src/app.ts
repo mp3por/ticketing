4dd269c3ab485bd46d916e24b438a91ff99ada11
Velin Kerkov <velin.kerkov@whitelotusorder.com>
2021-03-14 22:42:52 +0200

    end of CREATE-DESTROY server setup

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -1,30 +1,33 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-
-import {errorHandler, NotFoundError} from "@mp3por-tickets/common";
-import cookieSession from "cookie-session";
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@sgtickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
 app.use(
-    cookieSession({
-        signed: false,
-        secure: process.env.NODE_ENV !== 'test'
-    })
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
 );
+app.use(currentUser);
 
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
-    throw new NotFoundError();
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
 export { app };
-

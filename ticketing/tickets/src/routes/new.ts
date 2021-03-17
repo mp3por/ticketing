b36dcdec5de1f15129adfc6b33f5e43f4424b1da
Velin Kerkov <velin.kerkov@whitelotusorder.com>
2021-03-17 21:37:39 +0200

    progress

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -6,9 +6,9 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-const stan = nats.connect('ticketing', 'tickets', {
-  url: 'http://nats-srv:4222',
-});
+// const stan = nats.connect('ticketing', 'tickets', {
+//   url: 'http://nats-srv:4222',
+// });
 
 router.post(
   '/api/tickets',
@@ -31,13 +31,13 @@ router.post(
     });
     await ticket.save();
 
-    const event = {
-      type: 'ticket:created',
-      data: ticket,
-    };
-    stan.publish('ticket:created', JSON.stringify(event), () => {
-      console.log('Ticket creation event published');
-    });
+    // const event = {
+    //   type: 'ticket:created',
+    //   data: ticket,
+    // };
+    // stan.publish('ticket:created', JSON.stringify(event), () => {
+    //   console.log('Ticket creation event published');
+    // });
 
     res.status(201).send(ticket);
   }

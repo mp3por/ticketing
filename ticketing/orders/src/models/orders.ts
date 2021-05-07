ec3380629ea2425213755ebdb17a35b2dd4006d8
VelinKerkov <velin.kerkov@whitelotusorder.com>
2021-05-07 12:37:27 +0300

    Created Orders and Tickets models

---
+++ ticketing/orders/src/models/orders.ts
@@ -0,0 +1,59 @@
+import * as mongoose from "mongoose";
+import {OrderStatus} from "@mp3por-tickets/common";
+import {TicketDoc} from "./ticket";
+
+// what is needed to create an order
+interface OrderAttrs {
+    userId: string;
+    status: OrderStatus;
+    expiresAt: Date;
+    ticket: TicketDoc;
+}
+
+// how the order document looks like in the DB
+interface OrderDoc extends mongoose.Document {
+    userId: string;
+    status: OrderStatus;
+    expiresAt: Date;
+    ticket: TicketDoc;
+}
+
+// how the order model looks like
+interface OrderModel extends mongoose.Model<OrderDoc> {
+    build(attrs: OrderAttrs) : OrderDoc;
+}
+
+const ordersSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: Object.values(OrderStatus),
+        default: OrderStatus.Created
+    },
+    expiresAt: {
+        type: mongoose.Schema.Types.Date
+    },
+    ticket: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Ticket'
+    }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+        }
+    }
+})
+
+ordersSchema.statics.build = (attrs: OrderAttrs) => {
+    return new Order(attrs);
+}
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', ordersSchema);
+
+export { Order };

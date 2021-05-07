844292a8f9810cdf8afe78c911a6d2ac1c3978dc
VelinKerkov <velin.kerkov@whitelotusorder.com>
2021-05-07 12:00:28 +0300

    Implemented skaffolds for all route handlers

---
+++ ticketing/orders/src/routes/new.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from "express";
+import {requireAuth, validateRequest} from "@mp3por-tickets/common";
+import {body} from "express-validator";
+
+const router = express.Router();
+
+router.post(
+    '/api/orders',
+    [
+        requireAuth,
+        body('ticketId').not().isEmpty().withMessage('TicketId must be provided'),
+        validateRequest
+    ],
+    async (req: Request, res: Response) => {
+        res.send({})
+    });
+
+export {router as newOrderRouter};
